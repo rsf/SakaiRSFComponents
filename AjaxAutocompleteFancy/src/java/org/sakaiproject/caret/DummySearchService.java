045b404b101738dec234d4e3376816e87631d470
Antranig Basman <amb26@ponder.org.uk>
2008-01-11 14:36:31 +0000

    Updated to RSF 0.7.2 features - UIInitBlock, and template message keys, fixed build files

--- AjaxAutocompleteFancy/src/java/org/sakaiproject/caret/DummySearchService.java
+++ AjaxAutocompleteFancy/src/java/org/sakaiproject/caret/DummySearchService.java
@@ -9,65 +9,36 @@ import java.util.List;
 
 /**
  * A search service to let us pretend to be searching for a string among many
- *
+ * 
  * @author Aaron Zeckoski (aaronz@caret.cam.ac.uk)
  */
 public class DummySearchService {
 
-	private String[] searchValues = {
-		"aaronz",
-      "adam",
-		"alan",
-		"alexander",
-      "allison",
-      "amber",
-		"andy",
-		"anthony",
-		"antranig",
-      "anya",
-      "apple",
-      "ariel",
-      "arnold",
-      "azkaban",
-		"catharine",
-		"dan",
-		"daniel",
-		"emel",
-		"guy",
-		"harriet",
-		"ian",
-		"john",
-		"katy",
-		"matthew",
-		"mike",
-		"raad",
-		"raymond",
-		"rhiannon",
-		"rob",
-		"sarah",
-		"stephanie",
-		"steve",
-		"sue",
-		"sultan"
-	};
+  private String[] searchValues = { "aaronz", "adam", "alan", "alexander",
+      "allison", "amber", "andy", "anthony", "antranig", "anya", "apple",
+      "ariel", "arnold", "azkaban", "catharine", "dan", "daniel", "emel",
+      "guy", "harriet", "ian", "john", "katy", "matthew", "mike", "raad",
+      "raymond", "rhiannon", "rob", "sarah", "stephanie", "steve", "sue",
+      "sultan" };
 
-	/**
-	 * Return the array of all near matches for a search value
-	 * @param search a search string
-	 * @return the array of all near matches of the search prefix
-	 */
-	public String[] doSearch(String search) {
-		List<String> l = new ArrayList<String>();
-		for (int i = 0; i < searchValues.length; i++) {
-			if (searchValues[i].startsWith(search)) {
-				l.add( searchValues[i] );
-				// only return 5 results now
-				if(l.size() >= 5) {
-				   break;
-				}
-			}
-		}
-		return l.toArray( new String[] {} );
-	}
+  /**
+   * Return the array of all near matches for a search value
+   * 
+   * @param search a search string
+   * @return the array of all near matches of the search prefix
+   */
+  public String[] doSearch(String search) {
+    List<String> l = new ArrayList<String>();
+    for (int i = 0; i < searchValues.length; i++) {
+      if (searchValues[i].startsWith(search)) {
+        l.add(searchValues[i]);
+        // only return 5 results now
+        if (l.size() >= 5) {
+          break;
+        }
+      }
+    }
+    return l.toArray(new String[] {});
+  }
 
 }
